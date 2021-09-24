b7d0fd7c70ff4523cd570d7800b283eca2af91c1
Devin <devin.ray.broussard@gmail.com>
2021-09-24 09:24:52 -0500

    Created project

---
+++ Text-Based-Adventure/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_Based_Adventure
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ Text-Based-Adventure/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Text_Based_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of Game
+            Game game = new Game();
+
+            //Run the Game
+            game.Run();
+        }
+    }
+}
